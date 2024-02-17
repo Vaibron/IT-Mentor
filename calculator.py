018e946294fb3590cd332366ad23c67472070af9
Vaibron <160307500+Vaibron@users.noreply.github.com>
2024-02-17 09:51:08 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def main(input: str):
+    try:
+        inp = input.split()       
+        
+        if len(inp) != 3:
+            raise Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+        
+        a = int(inp[0])
+        b = int(inp[2])
+        c = inp[1]
+        
+        if a < 1 or a > 10 or b < 1 or b > 10:
+            raise Exception("Числа должны быть в диапазоне от 1 до 10.")
+    
+        if c == '+':
+            result = a + b
+        elif c == '-':
+            result = a - b
+        elif c == '*':
+            result = a * b
+        elif c == '/':
+            if b == 0:
+               raise Exception("Деление на ноль не допускается.")
+            result = a // b
+        else:
+            raise Exception("Недопустимая арифметическая операция. Используйте +, -, *, либо /.")
+            
+        return str(result)
+    
+    except Exception as e:
+        return str(e)
+        
+input_str = input()
+print(main(input_str))
