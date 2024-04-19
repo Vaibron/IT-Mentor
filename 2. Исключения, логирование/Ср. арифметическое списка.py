9f5fa05c2eaef32897012e7a025811c1933a511a
mr.vaibron <160307500+Vaibron@users.noreply.github.com>
2024-04-19 11:45:57 +0300

    Add files via upload

---
+++ 2. Исключения, логирование/Ср. арифметическое списка.py
@@ -0,0 +1,23 @@
+"""Написать программу для нахождения среднего арифметического списка чисел. 
+Если при вводе списка чисел была допущена ошибка (например, был передан не список, а строка), 
+программа должна корректно обработать эту ошибку и выдать соответствующее сообщение. 
+Информация об ошибках должна быть записана в лог."""
+
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="py_log.log", filemode="w" )
+
+err = 'При вводе списка чисел была допущена ошибка'
+
+def average(lst):
+    num  = sum(lst) / len(lst)
+    return num
+
+try:
+    lst = list(map(float, input(f'Введите числа через пробел').split()))
+    print(average(lst))
+except ValueError:
+    logging.error("Ошибка: в списоке имеются строки")
+    print(err)
+except ZeroDivisionError:
+    logging.error("Ошибка: список чисел пуст")
+    print(err)
