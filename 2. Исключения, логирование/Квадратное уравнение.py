9d9d5d4cd0cea27c0f2daa4a87c411f3df51e79d
aleksejsypko <mr.vaibron@gmail.com>
2024-04-22 12:32:25 +0300

    Uodate

--- 2. Исключения, логирование/Квадратное уравнение.py
+++ 2. Исключения, логирование/Квадратное уравнение.py
@@ -11,6 +11,7 @@ def quadratic_equation(a, b, c):
     D = b ** 2 - 4 * a * c
     try:
         if D > 0:
+            logging.warning('D > 0')
             x1 = (-b - D ** 0.5) / (2 * a)
             x2 = (-b + D ** 0.5) / (2 * a)
             return x1, x2
