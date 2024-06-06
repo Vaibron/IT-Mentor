27c18d79825906e0ab97987ce2cb506d2f6a95cc
aleksejsypko <mr.vaibron@gmail.com>
2024-06-06 15:32:55 +0300

    Update

---
+++ 6. ООП/task14.py
@@ -0,0 +1,33 @@
+'''Напишите класс, который принимает список людей с интерфейсом добавления новых
+и при итерации возвращал имена людей'''
+
+
+class PeopleList:
+    def __init__(self):
+        self.lst = []
+
+    def add_person(self, name):
+        self.lst.append(name)
+
+    def __iter__(self):
+        self.flag = 0
+        return self
+
+    def __next__(self):
+        if self.flag < len(self.lst):
+            county = self.lst[self.flag]
+            self.flag += 1
+            return county
+        else:
+            raise StopIteration
+
+
+people_list = PeopleList()
+people_list.add_person("Елизавета")
+people_list.add_person("Алексей")
+people_list.add_person("Роберт Оппенгеймер")
+
+pl = iter(people_list)
+print(next(pl))
+print(next(pl))
+print(next(pl))
---
+++ 6. ООП/task15.py
@@ -0,0 +1,49 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="mylog.log",
+    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+    datefmt='%H:%M:%S',
+)
+
+
+class LoggedAttributes:
+    def __init__(self, atr1, atr2, atr3):
+        self.atr1 = atr1
+        self.atr2 = atr2
+        self.atr3 = atr3
+
+    @property
+    def attr1(self):
+        return self.atr1
+
+    @attr1.setter
+    def attr1(self, value):
+        logging.info(f'atr1 изменен с {self.atr1} на {value}')
+        self.atr1 = value
+
+    @property
+    def attr2(self):
+        return self.atr2
+
+    @attr2.setter
+    def attr2(self, value):
+        logging.info(f'atr2 изменен с {self.atr2} на {value}')
+        self.atr2 = value
+
+    @property
+    def attr3(self):
+        return self.atr3
+
+    @attr3.setter
+    def attr3(self, value):
+        logging.info(f'atr3 изменен с {self.atr3} на {value}')
+        self.atr3 = value
+
+
+# Пример использования:
+obj = LoggedAttributes('Один', 'Два', 'Три')
+obj.attr1 = 'Четыре'
+obj.attr2 = 'Пять'
+obj.attr3 = 'Шесть'
